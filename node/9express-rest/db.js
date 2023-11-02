0d340760d1defd334e7546ee12859743a7b94c90
Alexey Svistunov <ansvistunov@gmail.com>
2023-11-02 22:29:27 +0300

    addd docker file for mysql database

---
+++ node/9express-rest/db.js
@@ -0,0 +1,22 @@
+var db = require("mysql");
+var connection = db.createConnection({
+    host:"localhost",
+    user:"alex",
+    password:"Qwerty123",
+    database:"world"
+});
+
+connection.connect(function(err){
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log("connection established");
+})
+
+
+
+
+
+
+module.exports = connection;
